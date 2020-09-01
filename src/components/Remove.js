bafee0979f9b5d00c503403d725fdf00b657036c
Douglas <douglaza@gmail.com>
2020-09-01 16:36:49 -0300

    CC doesn't like optional chaining

--- src/components/Remove.js
+++ src/components/Remove.js
@@ -4,7 +4,7 @@ import StarWarsContext from '../context/StarWarsContext';
 export default function Remove() {
   const { filters, deleteFilter } = useContext(StarWarsContext);
   const removeFilter = (selected) => deleteFilter(selected);
-  return filters.filterByNumericValues?.map((filtered) => (
+  return filters.filterByNumericValues.map((filtered) => (
     <div data-testid="filter" key={filtered.column}>
       <span>{`${filtered.column} - ${filtered.comparison} - ${filtered.value}`}</span>
       <button onClick={() => removeFilter(filtered)}>x</button>
