6a3c156952f75738fb376c0d74c687a4a7d6a457
Lucas Costa Ventura <lucascv333@gmail.com>
2020-09-01 23:31:04 -0300

    cc

--- src/components/Filtros.js
+++ src/components/Filtros.js
@@ -7,21 +7,21 @@ export default function Filtros() {
     <div>
       <h4>Filtros</h4>
       {filterByNumericValues.map((filtro) => (
-          <div key={filtro.column} data-testid="filter">
-            <span>{filtro.column}</span>
-            <span>{filtro.comparison}</span>
-            <span>{filtro.value}</span>
-            <button
-              name={filtro.column}
-              type="button"
-              onClick={(event) => {
-                setFiltros(filterByNumericValues.filter((filtro) => filtro.column !== event.target.name));
-              }}
-            >
-              X
-            </button>
-          </div>
-        ))}
+        <div key={filtro.column} data-testid="filter">
+          <span>{filtro.column}</span>
+          <span>{filtro.comparison}</span>
+          <span>{filtro.value}</span>
+          <button
+            name={filtro.column}
+            type="button"
+            onClick={(event) => {
+              setFiltros(filterByNumericValues.filter((fil) => fil.column !== event.target.name));
+            }}
+          >
+            X
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
