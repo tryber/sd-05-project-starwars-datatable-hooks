7903b0b67dd185299e19f78482b62b7fe4c7727a
Rafael Assad <rafaassadeng@gmail.com>
2020-09-02 10:19:20 -0300

    refatorando para o glorioso cc

--- src/component/SelectForm.js
+++ src/component/SelectForm.js
@@ -15,14 +15,8 @@ function TextForm() {
   } = useContext(StarWarsContext);
 
   const CO = ['Comparação', 'maior que', 'menor que', 'igual a'];
-  const colunas = [
-    'Coluna',
-    'population',
-    'orbital_period',
-    'diameter',
-    'rotation_period',
-    'surface_water',
-  ];
+  const colunas = ['Coluna', 'population', 'orbital_period', 'diameter', 'rotation_period',
+  'surface_water'];
 
   if (numberFilter.length > 0) {
     numberFilter.forEach((each) => {
@@ -32,31 +26,21 @@ function TextForm() {
 
   return (
     <div>
-      <Select
-        name="column"
-        onChange={(e) => setColumn(e.target.value)}
+      <Select name="column" onChange={(e) => setColumn(e.target.value)}
         id="column-filter"
         options={colunas}
       />
-      <Select
-        name="comparison"
-        onChange={(e) => setCO(e.target.value)}
+      <Select name="comparison" onChange={(e) => setCO(e.target.value)}
         id="comparison-filter"
         options={CO}
       />
       <label htmlFor="value">
-        <input
-          name="value"
-          type="number"
-          data-testid="value-filter"
+        <input name="value" type="number" data-testid="value-filter"
           onChange={(e) => setValue(e.target.value)}
         />
       </label>
-      <button
-        type="button"
-        data-testid="button-filter"
-        onClick={() => setNumberFilter([...numberFilter, { column, comparison, value }])}
-      >
+      <button type="button" data-testid="button-filter"
+        onClick={() => setNumberFilter([...numberFilter, { column, comparison, value }])}>
         Filtrar
       </button>
     </div>
--- src/services/planetsAPI.js
+++ src/services/planetsAPI.js
@@ -1,5 +1,5 @@
 export default function planetsAPI() {
   return fetch('https://swapi-trybe.herokuapp.com/api/planets/?format=json').then((response) =>
-    response.json()
+    response.json(),
   );
 }
