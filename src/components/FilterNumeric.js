7e8a075375429a2bef86abd5526540941b40460a
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-09-02 05:56:23 -0300

    ainda falta resolver warning

--- src/components/FilterNumeric.js
+++ src/components/FilterNumeric.js
@@ -1,66 +1,59 @@
 import React, { useContext, useState } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
+function DinamicChosenFilters() {
+  const { filterNumeric } = useContext(StarWarsContext);
+  const arrayColunas = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+  const arraySelected = [];
+  filterNumeric.forEach((item) => arraySelected.push(item.column));
+  const withOutSelected = arrayColunas.filter((item) => !arraySelected.includes(item));
+  return withOutSelected;
+}
+
 function FilterNumeric() {
   const [column, setColumn] = useState('');
   const [comparison, setComparison] = useState('');
   const [input, setInput] = useState('');
-
-  const onChangeHandle = (event) => {
-    const { name, value } = event.target;
-    if ( name === 'column') {
-      setColumn(value);
-    } else if (name === 'comparison') {
-      setComparison(value);
-    } else if(name === 'input') {
-      setInput(value);
-    }
-  }
+  const { filterNumeric, setFilterNumeric } = useContext(StarWarsContext);
+  const handleComparison = (e) => setComparison(e.target.value);
+  const handleColumn = (e) => setColumn(e.target.value);
+  const handleInput = (e) => setInput(e.target.value);
 
   const onClickHandle = () => {
     if (column === '' || comparison === '' || input === '') {
       alert('Não deixe campos em branco no filtro');
     } else {
-      setFilterNumeric([...filterNumeric, {column, comparison, value: input}]);
+      setFilterNumeric([...filterNumeric, { column, comparison, value: input }]);
       setColumn('');
       setComparison('');
       setInput('');
     }
-  }
+  };
 
-  const dinamicChosenFilters = () => {
-    const arrayColunas = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
-    const arraySelected = [];
-    filterNumeric.forEach(({ column }) => arraySelected.push(column));
-    const withOutSelected = arrayColunas.filter((item) => !arraySelected.includes(item));
-    return withOutSelected;
-  }
-    const { filterNumeric, setFilterNumeric } = useContext(StarWarsContext);
-    const arrayColunas = dinamicChosenFilters();
 
-    return (
-      <div>
-        <select data-testid="column-filter" onChange={onChangeHandle} name="column">
-          <option value="">Colunas</option>
-          {arrayColunas.map((item) => <option key={item} value={item}>{item}</option>)}
-        </select>
-        <select
-          data-testid="comparison-filter" value={comparison}
-          onChange={onChangeHandle} name="comparison"
-        >
-          <option value="">Comparação</option>
-          <option value="maior que">maior que</option>
-          <option value="menor que">menor que</option>
-          <option value="igual a">igual a</option>
-        </select>
-        <input
-          name="input" value={input} type="text"
-          data-testid="value-filter" onChange={onChangeHandle}
-        />
-
-        <button data-testid="button-filter" onClick={onClickHandle}>Filtrar</button>
-      </div>
-    );
+  const arrayColunas = DinamicChosenFilters();
+  return (
+    <div>
+      <select data-testid="column-filter" onChange={handleColumn} name="column">
+        <option value="">Colunas</option>
+        {arrayColunas.map((item) => <option key={item} value={item}>{item}</option>)}
+      </select>
+      <select
+        data-testid="comparison-filter" value={comparison}
+        onChange={handleComparison} name="comparison"
+      >
+        <option value="">Comparação</option>
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input
+        name="input" value={input} type="text"
+        data-testid="value-filter" onChange={handleInput}
+      />
+      <button data-testid="button-filter" onClick={onClickHandle}>Filtrar</button>
+    </div>
+  );
 }
 
 export default FilterNumeric;
--- src/components/Table.js
+++ src/components/Table.js
@@ -2,108 +2,108 @@ import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 import './Table.css';
 
+function dynamicsort(key, order) {
+  //  ref2 modificada.
+  let sortOrder = 1;
+  if (order === 'DESC') {
+    sortOrder = -1;
+  }
+  let varA;
+  let varB;
+
+  return function (a, b) {
+    if (key === 'population' || key === 'orbital_period' ||
+      key === 'diameter' || key === 'rotation_period' || key === 'surface_water') {
+      varA = parseInt(a[key], 10);
+      varB = parseInt(b[key], 10);
+    } else {
+      varA = a[key].toUpperCase();
+      varB = b[key].toUpperCase();
+    }
+
+    //  a should come before b in the sorted order
+    if (varA < varB) {
+      return -1 * sortOrder;
+    //  a should come after b in the sorted order
+    } else if (varA > varB) {
+      return 1 * sortOrder;
+    }
+    return 0;
+  };
+}
+
+function ManyFilter() {
+  const { data, filterParameter, filterNumeric, filterSortedColumn,
+    filterSortedOrder } = useContext(StarWarsContext);
+  const colunaSort = (filterSortedColumn.column === 'Name') ? 'name' : filterSortedColumn.column;
+  //  Ternário manobra técnica, pois há uma diferença entre o nome do campo e
+  //  a forma com o que store deve armazenar o column. (requisito dos testes)
+  let array = data.slice(); //  copia o conteudo do data, sem a referencia a data
+  //  assim, não se altera o estado devido a ligação por endereço (sem mutação)
+  array.sort(dynamicsort(colunaSort, filterSortedOrder.sort));
+  if (filterParameter.name !== '') {
+    array = data.filter((item) => item.name.includes(filterParameter.name));
+  }
+  if (filterNumeric.length > 0 && array.length > 1) {
+    filterNumeric.forEach(({ column, comparison, value }) => {
+      switch (comparison) {
+        case ('maior que'):
+          //  array.forEach((arrayItem) => console.log(typeof(arrayItem[column])));
+          array = array.filter((arrayItem) => parseInt(arrayItem[column], 10) > value);
+          break;
+        case ('menor que'):
+          array = array.filter((arrayItem) => parseInt(arrayItem[column], 10) < value);
+          break;
+        case ('igual a'):
+          array = array.filter((arrayItem) => arrayItem[column] === value);
+          break;
+        default:
+          break;
+      }
+    });
+  }
+  return array;
+}
+
+function RenderTableData() {
+  const array = ManyFilter();
+
+  return (array.map((item) => (
+    <tr key={item.name}>
+      <td data-testid="planet-name">{item.name}</td>
+      <td>{item.rotation_period}</td>
+      <td>{item.orbital_period}</td>
+      <td>{item.diameter}</td>
+      <td>{item.climate}</td>
+      <td>{item.gravity}</td>
+      <td>{item.terrain}</td>
+      <td>{item.surface_water}</td>
+      <td>{item.population}</td>
+      <td>{item.residents}</td>
+      <td>{item.films}</td>
+      <td>{item.created}</td>
+      <td>{item.edited}</td>
+    </tr>))
+  );
+}
+
 function Table() {
-
-  const dynamicsort = (key, order) => {
-    //  ref2 modificada.
-    let sortOrder = 1;
-    if (order === 'DESC') {
-      sortOrder = -1;
-    }
-    let varA;
-    let varB;
-
-    return function (a, b) {
-      if (key === 'population' || key === 'orbital_period' ||
-        key === 'diameter' || key === 'rotation_period' || key === 'surface_water') {
-        varA = parseInt(a[key], 10);
-        varB = parseInt(b[key], 10);
-      } else {
-        varA = a[key].toUpperCase();
-        varB = b[key].toUpperCase();
-      }
-
-      //  a should come before b in the sorted order
-      if (varA < varB) {
-        return -1 * sortOrder;
-      //  a should come after b in the sorted order
-      } else if (varA > varB) {
-        return 1 * sortOrder;
-      }
-      return 0;
-    };
-  }
-
- const manyFilter = () => {
-    const colunaSort = (filterSortedColumn.column === 'Name') ? 'name' : filterSortedColumn.column;
-    //  Ternário manobra técnica, pois há uma diferença entre o nome do campo e
-    //  a forma com o que store deve armazenar o column. (requisito dos testes)
-    let array = data.slice(); //  copia o conteudo do data, sem a referencia a data
-    //  assim, não se altera o estado devido a ligação por endereço (sem mutação)
-    array.sort(dynamicsort(colunaSort, filterSortedOrder.sort));
-    if (filterParameter.name !== '') {
-      array = data.filter((item) => item.name.includes(filterParameter.name));
-    }
-    if (filterNumeric.length > 0 && array.length > 1) {
-      filterNumeric.forEach(({ column, comparison, value }) => {
-        switch (comparison) {
-          case ('maior que'):
-            //  array.forEach((arrayItem) => console.log(typeof(arrayItem[column])));
-            array = array.filter((arrayItem) => parseInt(arrayItem[column], 10) > value);
-            break;
-          case ('menor que'):
-            array = array.filter((arrayItem) => parseInt(arrayItem[column], 10) < value);
-            break;
-          case ('igual a'):
-            array = array.filter((arrayItem) => arrayItem[column] === value);
-            break;
-          default:
-            break;
-        }
-      });
-    }
-    return array;
-  }
-  const RenderTableData = () => {
-    const array = manyFilter();
-
-    return (array.map((item) => (
-      <tr key={item.name}>
-        <td data-testid="planet-name">{item.name}</td>
-        <td>{item.rotation_period}</td>
-        <td>{item.orbital_period}</td>
-        <td>{item.diameter}</td>
-        <td>{item.climate}</td>
-        <td>{item.gravity}</td>
-        <td>{item.terrain}</td>
-        <td>{item.surface_water}</td>
-        <td>{item.population}</td>
-        <td>{item.residents}</td>
-        <td>{item.films}</td>
-        <td>{item.created}</td>
-        <td>{item.edited}</td>
-      </tr>))
+  const { data, isFetching } = useContext(StarWarsContext);
+  if (!isFetching && data.length > 1) {
+    const header = Object.keys(data[0]);
+    header.pop();
+    return (
+      <table className="tabela">
+        <thead>
+          <tr>{header.map((item) => <th key={item}>{item}</th>)}</tr>
+        </thead>
+        <tbody>
+          { RenderTableData() }
+        </tbody>
+      </table>
     );
   }
-
-  const { data, isFetching, filterParameter, filterNumeric, filterSortedColumn,
-    filterSortedOrder } = useContext(StarWarsContext);
-
-    if (!isFetching && data.length > 1) {
-      const header = Object.keys(data[0]);
-      header.pop();
-      return (
-        <table className="tabela">
-          <thead>
-            <tr>{header.map((item) => <th key={item}>{item}</th>)}</tr>
-          </thead>
-          <tbody>
-            { RenderTableData() }
-          </tbody>
-        </table>
-      );
-    }
-    return <div>loading...</div>;
-  }
+  return <div>loading...</div>;
+}
 
 export default Table;
