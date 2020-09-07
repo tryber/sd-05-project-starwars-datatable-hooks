8f4e65b63ed02e2d7ba0e5ac1e74dbab981353da
Preto115 <preto115@gmail.com>
2020-09-07 16:46:44 -0300

    ajustes finalizados CC

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,43 +1,49 @@
- import React, { useState } from 'react';
- import StarWarsContext from './StarWarsContext';
+import React, { useState } from 'react';
+import propTypes from 'prop-types';
+import StarWarsContext from './StarWarsContext';
 
-  const sortingColumn = (planets, sort, column, callback) => {
-    let testOrder = 1;
-    if (sort === 'DESC') testOrder = -1;
-    if (column === 'name' || column === 'climate' || column === 'edited' || column === 'gravity'
-    || column === 'terrain' || column === 'url' || column === 'films' || column === 'created') {
-      // funcao pra fazer um sort() em strings pura
-      planets.sort((a, b) => {
-        let x;
-        let y;
-        if (column === 'films') {
-          x = a[column].sort().reverse()[0].toLowerCase();
-          y = b[column].sort().reverse()[0].toLowerCase();
-        } else {
-          x = a[column].toLowerCase();
-          y = b[column].toLowerCase();
-        }
+function sortingString(column, planets, testOrder) {
+  const planet = planets.sort((a, b) => {
+    let x;
+    let y;
+    // if (column === 'films') {
+    //   // x = a[column].sort().reverse()[0].toLowerCase();
+    //   // y = b[column].sort().reverse()[0].toLowerCase();
+    // } else {
+      x = a[column].toLowerCase();
+      y = b[column].toLowerCase();
+    // }
 
-        if (x < y) {
-          return -1 * testOrder;
-        }
-        if (x > y) {
-          return 1 * testOrder;
-        }
-        return 0;
-      });
-      callback([...planets]);
+    if (x < y) {
+      return -1 * testOrder;
     }
-
-    if (sort === 'ASC') {
-      callback([...planets.sort((a, b) => (a[column] - b[column]))]);
-    } // aqui sort() pra comparar string numeros dependendo do sort attribute( ASD, DESC)
-    if (sort === 'DESC') {
-        callback([...planets.sort((a, b) => (b[column] - a[column]))]);
-      }
+    if (x > y) {
+      return 1 * testOrder;
     }
- 
- function Provider(props) {
+    return 0;
+  });
+ return planet;
+}
+
+const sortingColumn = (planets, sort, column, callback) => {
+  let testOrder = 1;
+  if (sort === 'DESC') testOrder = -1;
+  if (column === 'name' || column === 'climate' || column === 'edited' || column === 'gravity'
+  || column === 'terrain' || column === 'url' || column === 'films' || column === 'created') {
+    // funcao pra fazer um sort() em strings pura
+    const planet = sortingString(column, planets, testOrder);
+    callback([...planet]);
+  }
+
+  if (sort === 'ASC') {
+    callback([...planets.sort((a, b) => (a[column] - b[column]))]);
+  } // aqui sort() pra comparar string numeros dependendo do sort attribute( ASD, DESC)
+  if (sort === 'DESC') {
+    callback([...planets.sort((a, b) => (b[column] - a[column]))]);
+  }
+};
+
+function Provider(props) {
   const [data, setData] = useState([]);
   const [error, setError] = useState('');
   const [isFetching, setIsFetching] = useState(true);
@@ -45,33 +51,49 @@
   const [filterByName, setFilterByName] = useState({ name: '' });
   const [currentFilter, setCurrentFilter] = useState({ column: '', comparison: '', value: '' });
   const [order, setOrder] = useState({ column: 'Name', sort: 'ASD' });
-  
-  function addFilter ( filter ) {
-  setFilterByNumericValues([...filterByNumericValues, filter])
+
+  function addFilter(filter) {
+    setFilterByNumericValues([...filterByNumericValues, filter]);
   }
 
-  const excludeFilterButton = (id) => (
-    setFilterByNumericValues([...filterByNumericValues.filter(({ column }) => column !== id)])
-  )
+  function excludeFilterButton(id) {
+    setFilterByNumericValues([...filterByNumericValues.filter(({ column }) => column !== id)]);
+  }
 
-    const contextValue = {
-      data, setData,
-      error, setError,
-      isFetching, setIsFetching,
-      filterByNumericValues, setFilterByNumericValues,
-      filterByName, setFilterByName,
-      currentFilter, setCurrentFilter,
-      order, setOrder,
-      addFilter,
-      excludeFilterButton,
-      sortingColumn,
-    };
-     
-    return(
-      <StarWarsContext.Provider value={contextValue}>
-        {props.children}
-      </StarWarsContext.Provider>
-    );
+  const contextValue = {
+    data,
+    setData,
+    error,
+    setError,
+    isFetching,
+    setIsFetching,
+    filterByNumericValues,
+    setFilterByNumericValues,
+    filterByName,
+    setFilterByName,
+    currentFilter,
+    setCurrentFilter,
+    order,
+    setOrder,
+    addFilter,
+    excludeFilterButton,
+    sortingColumn,
+  };
+
+  return (
+    <StarWarsContext.Provider value={contextValue}>
+      {props.children}
+    </StarWarsContext.Provider>
+  );
 }
 
 export default Provider;
+
+
+Provider.propTypes = {
+  children: propTypes.objectOf(Object).isRequired,
+  // planets: propTypes.arrayOf(propTypes.object),
+  // sort: propTypes.string.isRequired,
+  // column: propTypes.string.isRequired,
+
+};
