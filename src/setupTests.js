76486478d2275d86a653296a8ebafa947d665741
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-09-08 17:59:10 -0300

    só pra mudar de branch

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,4 +5,4 @@
 import '@testing-library/jest-dom/extend-expect';
 import MutationObserver from '@sheerun/mutationobserver-shim';
 
-window.MutationObserver = MutationObserver;
+// window.MutationObserver = MutationObserver;
