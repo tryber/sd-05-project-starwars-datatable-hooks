bcc9e9bac996db03e2854f55a7190aaa54df214f
Mau <mauricio.sdjr@gmail.com>
2020-09-08 14:05:50 -0300

    codeclimate

--- src/components/NumericFIlters.jsx
+++ src/components/NumericFIlters.jsx
@@ -5,18 +5,29 @@ export default function NumericFilters() {
   const { column, setColumn, comparison, setComparison, value, setValue, setFilterByNumbers } = useContext(StarWarsContext);
   const options = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
   const options2 = ['menor que', 'maior que', 'igual a'];
-    return (
-      <div>
-        <select data-testid="column-filter" value={column} onChange={(e) => setColumn(e.target.value)}>
-          <option defaultValue>Selecione</option>
-          {options.map((opcao) => (<option>{opcao}</option>))}
-        </select>
-        <select data-testid="comparison-filter" value={comparison} onChange={(e) => setComparison(e.target.value)}>
-          <option defaultValue>Selecione</option>
-          {options2.map((opcao) => (<option>{opcao}</option>))}
-        </select>
-        <input data-testid="value-filter" type="number" value={value} onChange={(e) => setValue(e.target.value)} />
-        <button type="button" data-testid="button-filter" onClick={() => setFilterByNumbers([{ column, comparison, value }])}>Filtrar</button>
-      </div>
+  return (
+    <div>
+      <select
+        data-testid="column-filter"
+        value={column}
+        onChange={(e) => setColumn(e.target.value)}>
+        <option defaultValue>Selecione</option>
+        {options.map((opcao) => (<option>{opcao}</option>))}
+      </select>
+      <select
+        data-testid="comparison-filter"
+        value={comparison} onChange={(e) => setComparison(e.target.value)}>
+        <option defaultValue>Selecione</option>
+        {options2.map((opcao) => (<option>{opcao}</option>))}
+      </select>
+      <input
+        data-testid="value-filter"
+        type="number" value={value}
+        onChange={(e) => setValue(e.target.value)} />
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={() => setFilterByNumbers([{ column, comparison, value }])}>Filtrar</button>
+    </div>
   );
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,27 +3,33 @@ import StarWarsContext from '../context/StarWarsContext';
 import planetAPI from '../services/api';
 import TableHead from './TableHead';
 
+function allFilters(data, filter) {
+  if (filter.comparison === 'maior que') {
+    return data.filter((planet) => Number(planet[filter.column]) > Number(filter.value));
+  }
+  if (filter.comparison === 'menor que') {
+    return data.filter((planet) => Number(planet[filter.column]) < Number(filter.value));
+  }
+  if (filter.comparison === 'igual a') {
+    return data.filter((planet) => Number(planet[filter.column]) === Number(filter.value));
+  }
+  return data;
+}
+
 export default function Table() {
-  const { isFetching, setIsFetching, data, setData, name, filterByNumbers } = useContext(StarWarsContext);
+  const { isFetching,
+    setIsFetching,
+    data,
+    setData,
+    name,
+    filterByNumbers
+  } = useContext(StarWarsContext);
 
   useEffect(() => {
     setIsFetching(true);
     planetAPI().then((response) => setData(response.results));
     setIsFetching(false);
   }, [setIsFetching, setData]);
-
-  function allFilters(data, filter) {
-    if (filter.comparison === 'maior que') {
-      return data.filter((planet) => Number(planet[filter.column]) > Number(filter.value));
-    }
-    if (filter.comparison === 'menor que') {
-      return data.filter((planet) => Number(planet[filter.column]) < Number(filter.value));
-    }
-    if (filter.comparison === 'igual a') {
-      return data.filter((planet) => Number(planet[filter.column]) === Number(filter.value));
-    }
-    return data;
-  }
 
   let planets = data;
   filterByNumbers.forEach((filter) => { planets = allFilters(planets, filter); });
