fa4b81ea9760ed73d0ba0ca5f2c43d2a37fc8567
Sidnei Ramos <sidneiramos02@gmail.com>
2020-09-27 23:01:18 -0300

    Busca por texto implementada

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import StarWarsProvider from './context/StarWarsContext';
 import Table from './components/Table';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <StarWarsProvider>
+      <SearchBar />
       <Table />
     </StarWarsProvider>
   );
---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { StarWarsContext } from '../context/StarWarsContext';
+// import SelectOption from './SelectOption';
+
+function SearchBar() {
+  const {setFilterByName} = useContext(StarWarsContext)
+  return (
+    <div>
+      <input
+        data-testid="name-filter"
+        onChange={(event) => setFilterByName(event.target.value)}
+      />
+      {/* <SelectOption /> */}
+    </div>
+  );
+}
+
+// const mapDispatchToProps = (dispatch) => ({
+//   filterByName: (name) => dispatch(filterPlanet(name)),
+// });
+
+export default SearchBar;
+
+// SearchBar.propTypes = {
+//   filterByName: PropTypes.func.isRequired,
+// };
