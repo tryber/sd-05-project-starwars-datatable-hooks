d372b87ad199acd4b2caceae3de3fe21aca12685
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-09-30 23:49:50 -0300

    CC

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
 const store = createStore(
   rootReducer,
   composeEnhancers(
