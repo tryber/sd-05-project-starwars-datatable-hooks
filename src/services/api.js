3389bdf58fd10dd99d81972a15cc9d426647aa09
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-10-04 12:07:53 -0300

    more cc fixes

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,16 @@
 const url = 'https://swapi-trybe.herokuapp.com/api/planets';
 
-//codigo elaborado junto com Hamaji e Luis Medeiros e PR do rafaelqleite
-//uso de try-catch para lidar com erros
-//função assincrona com o uso de await para promises usadas
+// codigo elaborado junto com Hamaji e Luis Medeiros e PR do rafaelqleite
+// uso de try-catch para lidar com erros
+// função assincrona com o uso de await para promises usadas
 
 export default async function getPlanets() {
-  try { 
+  try {
     const response = await fetch(url);
     const data = await response.json();
     return data;
-  } catch (error){
+  } catch (error) {
     console.log(error);
+    return;
   }
 }
