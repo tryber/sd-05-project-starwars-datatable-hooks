af6b08971a3aa6bd2cb1186a683495fcb6cc2282
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-10-26 18:07:03 -0300

    codeclimate fixes

--- src/components/FilterDisplay.jsx
+++ src/components/FilterDisplay.jsx
@@ -1,30 +1,35 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 const FilterDisplay = () => {
-  const { numericFilter, setNumericFilter, controller, setController, } = useContext(StarWarsContext);
+  const {
+    numericFilter,
+    setNumericFilter,
+    controller,
+    setController
+  } = useContext(StarWarsContext);
 
   const removeObj = (filter) => {
-      const name = filter.column;
-      setNumericFilter(numericFilter.filter((e)=>(e.column !== name)));
-      switch (name) {
-        case 'rotation_period':
-          setController({ ...controller, rotation_period: true });
-          break;
-        case 'orbital_period':
-          setController({ ...controller, orbital_period: true });
-          break;
-        case 'diameter':
-          setController({ ...controller, diameter: true });
-          break;
-        case 'surface_water':
-          setController({ ...controller, surface_water: true });  
+    const name = filter.column;
+    setNumericFilter(numericFilter.filter((e) => (e.column !== name)));
+    switch (name) {
+      case 'rotation_period':
+        setController({ ...controller, rotation_period: true });
         break;
-          case 'population':
-            setController({ ...controller, population: true });
-            break;
-        default:
-          console.log(`Sorry, no match found...`);
+      case 'orbital_period':
+        setController({ ...controller, orbital_period: true });
+        break;
+      case 'diameter':
+        setController({ ...controller, diameter: true });
+        break;
+      case 'surface_water':
+        setController({ ...controller, surface_water: true });  
+      break;
+        case 'population':
+        setController({ ...controller, population: true });
+        break;
+      default:
+        console.log(`Sorry, no match found...`);
       }
       };
     return (
