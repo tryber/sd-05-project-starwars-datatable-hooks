12bb8bd5a48a80a50592a67bf97beffe83732089
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-10-27 13:51:45 -0300

    testing requirements

--- src/components/NumericFilter.jsx
+++ src/components/NumericFilter.jsx
@@ -2,13 +2,9 @@ import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 const NumericFilter = () => {
-  const { column, comparison, value,
-    numericFilter, setNumericFilter,
-    controller, setController,
-    filterColumns, setColumn,
-    setComparison, setValue,
+  const { column, comparison, value, numericFilter, setNumericFilter, controller, setController,
+    filterColumns, setColumn, setComparison, setValue,
   } = useContext(StarWarsContext);
-
   const handleColumn = (e) => setColumn(e.target.value);
   const handleComparison = (e) => setComparison(e.target.value);
   const handleValue = (e) => setValue(e.target.value);
@@ -27,21 +23,28 @@ const NumericFilter = () => {
         <option value="">Colunas</option>
         {filterColumns.map((item) => <option key={item} value={item}>{item}</option>)}
       </select>
-
-      <select data-testid="comparison-filter" /* value={} */
-      onChange={handleComparison} name="comparison">
+      <select
+        data-testid="comparison-filter"
+        /* value={} */
+        onChange={handleComparison}
+        name="comparison"
+      >
         <option value="">Comparação</option>
         <option value="maior que">maior que</option>
         <option value="menor que">menor que</option>
         <option value="igual a">igual a</option>
       </select>
-
-      <input name="input" /* value={} */ type="text"
-      data-testid="value-filter" onChange={handleValue} />
-
-      <button data-testid="button-filter" onClick={ClickHandler}>
-        Filtrar
-      </button>
+      <input
+        name="input"
+        /* value={} */
+        type="text"
+        data-testid="value-filter"
+        onChange={handleValue}
+      />
+      <button
+        data-testid="button-filter"
+        onClick={ClickHandler}
+      >Filtrar</button>
     </div>
   );
 };
