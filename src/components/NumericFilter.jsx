923bf09bec6c505147c363f5abeec545fdb1e802
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-10-27 13:31:30 -0300

    codeclimate

--- src/components/NumericFilter.jsx
+++ src/components/NumericFilter.jsx
@@ -2,18 +2,11 @@ import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 const NumericFilter = () => {
-  const {
-    column,
-    comparison,
-    value,
-    numericFilter,
-    setNumericFilter,
-    controller,
-    setController,
-    filterColumns,
-    setColumn,
-    setComparison,
-    setValue,
+  const { column, comparison, value,
+    numericFilter, setNumericFilter,
+    controller, setController,
+    filterColumns, setColumn,
+    setComparison, setValue,
   } = useContext(StarWarsContext);
 
   const handleColumn = (e) => setColumn(e.target.value);
@@ -21,52 +14,30 @@ const NumericFilter = () => {
   const handleValue = (e) => setValue(e.target.value);
   const ClickHandler = () => {
     const col = { ...{ column } };
-    if (controller[col.column] === true){
+    if (controller[col.column] === true) {
       setNumericFilter([...numericFilter, {
         column, comparison, value,
       }]);
-      setController({...controller, [column]: false });  
+      setController({ ...controller, [column]: false });
     } else { alert('filtro repetido'); }
-  }
+  };
   return (
     <div>
-      <select
-        data-testid="column-filter"
-        onChange={handleColumn}
-        name="column"
-      >
+      <select data-testid="column-filter" onChange={handleColumn} name="column">
         <option value="">Colunas</option>
         {filterColumns.map((item) => <option key={item} value={item}>{item}</option>)}
       </select>
 
-      <select
-        data-testid="comparison-filter"
-        /*
-        value={}
-        */
-        onChange={handleComparison}
-        name="comparison"
-      >
+      <select data-testid="comparison-filter" /* value={} */ onChange={handleComparison} name="comparison">
         <option value="">Comparação</option>
         <option value="maior que">maior que</option>
         <option value="menor que">menor que</option>
         <option value="igual a">igual a</option>
       </select>
 
-      <input
-        name="input"
-        /*
-        value={}
-        */
-        type="text"
-        data-testid="value-filter"
-        onChange={handleValue}
-      />
+      <input name="input" /* value={} */ type="text" data-testid="value-filter" onChange={handleValue} />
 
-      <button
-        data-testid="button-filter"
-        onClick={ClickHandler}
-      >
+      <button data-testid="button-filter" onClick={ClickHandler}>
         Filtrar
       </button>
     </div>
