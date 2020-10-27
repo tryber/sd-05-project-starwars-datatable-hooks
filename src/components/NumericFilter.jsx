0960ad3136256e2a4fd62a7566d0df79e455780e
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-10-27 09:50:23 -0300

    codeclimate fixes

--- src/components/NumericFilter.jsx
+++ src/components/NumericFilter.jsx
@@ -11,46 +11,55 @@ const NumericFilter = () => {
   const handleComparison = (e) => setComparison(e.target.value);
   const handleValue = (e) => setValue(e.target.value);
 
-  function clickHandler() {
-    if (column === 'rotation_period') {
-      if (controller.rotation_period === true) {
-        setNumericFilter([...numericFilter, {
-          column, comparison, value,
-        }]);
-        setController({ ...controller, rotation_period: false });
-      } else { alert('filtro repetido'); }
-    }
-    if (column === 'orbital_period') {
-      if (controller.orbital_period === true) {
-        setNumericFilter([...numericFilter, {
-          column, comparison, value,
-        }]);
-        setController({ ...controller, orbital_period: false });
-      } else { alert('filtro repetido'); }
-    }
-    if (column === 'diameter') {
-      if (controller.diameter === true) {
-        setNumericFilter([...numericFilter, {
-          column, comparison, value,
-        }]);
-        setController({ ...controller, diameter: false });
-      } else { alert('filtro repetido'); }
-    }
-    if (column === 'surface_water') {
-      if (controller.surface_water === true) {
-        setNumericFilter([...numericFilter, {
-          column, comparison, value,
-        }]);
-        setController({ ...controller, surface_water: false });
-      } else { alert('filtro repetido'); }
-    }
-    if (column === 'population') {
-      if (controller.population === true) {
-        setNumericFilter([...numericFilter, {
-          column, comparison, value,
-        }]);
-        setController({ ...controller, population: false });
-      } else { alert('filtro repetido'); }
+  const clickHandler = () => {
+    switch (column) {
+      case 'rotation_period':
+        if (controller.rotation_period === true) {
+          setNumericFilter([...numericFilter, {
+            column, comparison, value,
+          }]);
+          setController({ ...controller, rotation_period: false });
+        } else { alert('filtro repetido'); }
+    
+        break;
+      case 'orbital_period':
+        if (controller.orbital_period === true) {
+          setNumericFilter([...numericFilter, {
+            column, comparison, value,
+          }]);
+          setController({ ...controller, orbital_period: false });
+        } else { alert('filtro repetido'); }
+        
+        break;
+      case 'diameter':
+        if (controller.diameter === true) {
+          setNumericFilter([...numericFilter, {
+            column, comparison, value,
+          }]);
+          setController({ ...controller, diameter: false });
+        } else { alert('filtro repetido'); }
+
+        break;
+        case 'surface_water':
+          if (controller.surface_water === true) {
+            setNumericFilter([...numericFilter, {
+              column, comparison, value,
+            }]);
+            setController({ ...controller, surface_water: false });
+          } else { alert('filtro repetido'); }
+          
+          break;
+        case 'population':
+          if (controller.population === true) {
+            setNumericFilter([...numericFilter, {
+            column, comparison, value,
+            }]);
+            setController({ ...controller, population: false });
+          } else { alert('filtro repetido'); }
+      
+        break;
+      default: console.log('Sorry, no match found...');
+        break;
     }
   }
   return (
