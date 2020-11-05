2172858d6c860aaadfcb6dee26d3ee2e2f34e761
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-11-05 14:43:26 -0300

    getPlanets refactoring

--- src/services/api.js
+++ src/services/api.js
@@ -4,13 +4,31 @@ const url = 'https://swapi-trybe.herokuapp.com/api/planets';
 // uso de try-catch para lidar com erros
 // função assincrona com o uso de await para promises usadas
 
-export default async function getPlanets() {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-    return null;
+// export default async function getPlanets() {
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//     return null;
+//   }
+// }
+
+//sugestão do Vitor Caciquinho
+async function getPlanets() {
+  const url = 'https://swapi-trybe.herokuapp.com/api/planets';
+    try {
+      const response = await fetch(url);
+      
+      if(!response.ok){
+              throw new Error("erro")
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
   }
-}
+  export default getPlanets;
