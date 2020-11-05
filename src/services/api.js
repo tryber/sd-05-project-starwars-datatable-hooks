ca18b972c93b1cd05697fe48655dc93286ed0b53
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-11-05 14:50:59 -0300

    codeclimate

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 const url = 'https://swapi-trybe.herokuapp.com/api/planets';
 
+const url = 'https://swapi-trybe.herokuapp.com/api/planets';
 // codigo elaborado junto com Hamaji e Luis Medeiros e PR do rafaelqleite
 // uso de try-catch para lidar com erros
 // função assincrona com o uso de await para promises usadas
@@ -15,20 +16,18 @@ const url = 'https://swapi-trybe.herokuapp.com/api/planets';
 //   }
 // }
 
-//sugestão do Vitor Caciquinho
+// sugestão do Vitor Caciquinho
 async function getPlanets() {
-  const url = 'https://swapi-trybe.herokuapp.com/api/planets';
-    try {
-      const response = await fetch(url);
-      
-      if(!response.ok){
-              throw new Error("erro")
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-      return error;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('erro');
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return error;
   }
-  export default getPlanets;
+}
+export default getPlanets;
