e4b9b37399e50ef37ab8433efd908d9523206090
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-11-10 10:25:46 -0300

    #caciquez requisitos locais 100%

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -41,7 +41,7 @@ const TableBody = () => {
     planetasDoHugo.filter((planets) => (filterName !== '' ? planets.name.toLowerCase().includes(filterName) : data)).map(
       (planet) => (
         <tr key={planet.name}>
-          <td>{planet.name}</td><td>{planet.rotation_period}</td><td>{planet.orbital_period}</td>
+          <td data-testid="planet-name">{planet.name}</td><td>{planet.rotation_period}</td><td>{planet.orbital_period}</td>
           <td>{planet.diameter}</td><td>{planet.climate}</td><td>{planet.gravity}</td>
           <td>{planet.terrain}</td><td>{planet.surface_water}</td><td>{planet.population}</td>
           <td>{planet.films.map((film) => (<span key={film}>{film}</span>))}</td>
