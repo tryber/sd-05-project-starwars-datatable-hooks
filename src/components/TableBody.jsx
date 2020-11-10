e8d594601032d2f09cbcda5012f36fd3253f4075
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-11-10 10:47:29 -0300

    codeclimate  requisitos locais 100%

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -42,9 +42,10 @@ const TableBody = () => {
       (planet) => (
         <tr key={planet.name}>
           <td data-testid="planet-name">{planet.name}</td><td>{planet.rotation_period}</td>
-          <td>{planet.orbital_period}</td><td>{planet.diameter}</td><td>{planet.climate}</td><td>{planet.gravity}</td>
-          <td>{planet.terrain}</td><td>{planet.surface_water}</td><td>{planet.population}</td>
-          <td>{planet.films.map((film) => (<span key={film}>{film}</span>))}</td>
+          <td>{planet.orbital_period}</td><td>{planet.diameter}</td><td>{planet.climate}</td>
+          <td>{planet.gravity}</td><td>{planet.terrain}</td><td>{planet.surface_water}</td>
+          <td>{planet.population}</td><td>{planet.films.map(
+            (film) => (<span key={film}>{film}</span>))}</td>
           <td>{planet.created}</td><td>{planet.edited}</td><td>{planet.url}</td>
         </tr>
       ),
