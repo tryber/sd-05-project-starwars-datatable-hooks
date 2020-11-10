af6a8b4622329f1d6602b609176d3a6e523c91c0
Larissa Palombo <larissampalombo@gmail.com>
2020-11-10 15:01:24 -0300

    cc do comentario

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Table from './components/Table';
 import Provider from './context/Provider';
 
 // The Provider component is what makes the state available to all child components,
-// no matter how deeply nested they are within the component hierarchy. 
-// The Provider component receives a value prop. 
+// no matter how deeply nested they are within the component hierarchy.
+// The Provider component receives a value prop.
 // This is where we’ll pass our current value:
 function App() {
   return (
