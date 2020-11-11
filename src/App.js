41c19b26207a53913c6d11b49801d8066c7c24cf
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-11-11 09:12:05 -0300

    desmontando codigo para verificar requisitos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Provider from './service/Provider';
-import FilterName from './components/FilterName';
-import NumericFilter from './components/NumericFilter';
+// import FilterName from './components/FilterName';
+// import NumericFilter from './components/NumericFilter';
 import Table from './components/Table';
 import './App.css';
-import FilterDisplay from './components/FilterDisplay';
-import FilterOrder from './components/FilterOrder';
+// import FilterDisplay from './components/FilterDisplay';
+// import FilterOrder from './components/FilterOrder';
 
 function App() {
   return (
     <Provider>
-      <FilterName />
-      <NumericFilter />
-      <FilterDisplay />
-      <FilterOrder />
+      {/* <FilterName /> */}
+      {/* <NumericFilter /> */}
+      {/* <FilterDisplay /> */}
+      {/* <FilterOrder /> */}
       <Table />
     </Provider>
   );
